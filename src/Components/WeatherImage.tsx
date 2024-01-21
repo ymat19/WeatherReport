33f0a4ee25584ed6c9c00cd887598cbb3e936aea
ymat19 <af15081@shibaura-it.ac.jp>
2024-01-21 17:35:12 +0900

    裏で動画再生

--- src/Components/WeatherImage.tsx
+++ src/Components/WeatherImage.tsx
@@ -18,9 +18,36 @@ export const WeatherImage = ({ isRainy }: Props) => {
   }, []);
   console.log(isRainy);
 
-  return isRainy ? (
-    <img src={`${import.meta.env.BASE_URL}/5000choyen.png`} />
-  ) : (
-    <Clock renderMinuteMarks={false} renderSecondHand={false} value={value} />
+  return (
+    <>
+      <div style={{ position: "relative" }}>
+        {isRainy ? (
+          <img src={`${import.meta.env.BASE_URL}/5000choyen.png`} />
+        ) : (
+          <Clock
+            renderMinuteMarks={false}
+            renderSecondHand={false}
+            value={value}
+          />
+        )}
+      </div>
+      <video
+        className="video"
+        id="video"
+        loop
+        style={{
+          position: "absolute",
+          zIndex: -1,
+          opacity: 0,
+          width: "100%", // 必要に応じてサイズを調整してね♥
+          height: "100%", // これもね♥
+        }}
+      >
+        <source
+          src={`${import.meta.env.BASE_URL}/sample-5s.mp4`}
+          type="video/mp4"
+        />
+      </video>
+    </>
   );
 };
